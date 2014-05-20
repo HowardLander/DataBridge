252df46aadbd1d6d3e708886ef83121089ff258a
Michael Shoffner <shoffner@databridge.renci.org>
2014-05-20 16:19:13 -0400

    Removed server..Version.java.

--- server/src/main/java/org/renci/databridge/Version.java
+++
@@ -1,6 +0,0 @@
-package org.renci.databridge;
-public final class Version {
- public static String VERSION="1.0-BETA";
- public static String DATABRIDGE_VERSION=".1-SNAPSHOT";
- public static String BUILD_TIME="2014-04-25 04:38";
-}
