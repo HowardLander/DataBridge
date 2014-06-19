243828b15c038e62514fb2cbfd466ecba735f7d6
Howard Lander <howard@renci.org>
2014-06-19 13:20:26 -0400

    Doesn't break the build.

--- engines/relevance/src/test/java/org/renci/databridge/engines/RelevanceEngine.java
+++ engines/relevance/src/test/java/org/renci/databridge/engines/RelevanceEngine.java
@@ -25,7 +25,7 @@ public class RelevanceEngine
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( RelevanceEngine.class );
     }
 
     /**
