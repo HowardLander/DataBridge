ed1f2602666af17ebae60b030932e0f15386d7b2
Howard Lander <howard@renci.org>
2015-04-07 17:29:45 -0400

    Updated the documentation

--- persistence/src/main/java/org/renci/databridge/persistence/metadata/SNAInstanceDAO.java
+++ persistence/src/main/java/org/renci/databridge/persistence/metadata/SNAInstanceDAO.java
@@ -2,15 +2,59 @@ package org.renci.databridge.persistence.metadata;
 import  java.util.*;
 
 public interface SNAInstanceDAO {
+    /**
+     *  Insert the specified SNA instance into the database
+     *  @param theSNAInstance The SNA Instance to insert.
+     *  @return true on success, false or failure
+     */
     public boolean insertSNAInstance(SNAInstanceTransferObject theSNAInstance);
+    /**
+      *  Update the sna instance specified by the search map with the data in the update map
+      *  @param searchMap The HashMap containing the search criteria
+      *  @param updateMap The HashMap containing the update info
+      *  @return boolean for success or failure.
+      */
     public boolean updateSNAInstance(HashMap<String, String> searchMap, HashMap<String, String> updateMap);
+    /**
+      *  Update the sna instance specified by the transfer object with the data in the update map
+      *  @param theSNAInstance The transfer object containing the search criteria
+      *  @param updateMap The HashMap containing the update info
+      *  @return boolean for success or failure.
+      */
     public boolean updateSNAInstance(SNAInstanceTransferObject theSNAInstance, 
                                      HashMap<String, String> updateMap);
+    /**
+      *  Retrieve an iterator for all the SNA instances in the search map.
+      *  @param searchMap The HashMap containing the search criteria.
+      *  @return the requested iterator
+      */
     public Iterator<SNAInstanceTransferObject> getSNAInstances(HashMap<String, String> searchMap);
+    /**
+      *  Retrieve an iterator for all the SNA instances in the search map.
+      *  @param searchMap The HashMap containing the search criteria.
+      *  @param sortMap The HashMap containing the sorting criteria
+      *  @param limit The max number of objects to return
+      *  @return the requested iterator
+      */
     public Iterator<SNAInstanceTransferObject> getSNAInstances(HashMap<String, String> searchMap,
                                                                HashMap<String, String> sortMap, Integer limit);
+    /**
+      *  Retrieve the SNAInstanceTransferObject that matches the specified id
+      *  @param id the id for the SNA Instance to retrieve.
+      *  @return the requested SNAInstanceTransferObject or null
+      */
     public SNAInstanceTransferObject getSNAInstanceById(String id);
-    public int deleteSNAInstance(SNAInstanceTransferObject theSimilarityInstance);
+    /**
+     *  Delete the SNA instance specified in the SimilarityInstanceTransferObject.
+     *  @param theSNAInstance The SNAInstanceTransferObject to delete.
+     *  @return the number of objects deleted.
+     */
+    public int deleteSNAInstance(SNAInstanceTransferObject theSNAInstance);
+    /**
+     *  Delete the sna instance selected by the search map.
+     *  @param searchMap The HashMap containing the search criteria.
+     *  @return the number of objects deleted.
+     */
     public int deleteSNAInstance(HashMap<String, String> searchMap);
     public static final String SORT_ASCENDING = "ascending";
     public static final String SORT_DESCENDING = "descending";
--- persistence/src/main/java/org/renci/databridge/persistence/metadata/VariableDAO.java
+++ persistence/src/main/java/org/renci/databridge/persistence/metadata/VariableDAO.java
@@ -2,12 +2,49 @@ package org.renci.databridge.persistence.metadata;
 import  java.util.*;
 
 public interface VariableDAO {
+    /**
+     *  Insert the specified variable into the database
+     *  @param theVariable The VariableTransferObject to insert.
+     *  @return true on success, false or failure
+     */
     public boolean insertVariable(VariableTransferObject theVariable);
+    /**
+     *  Insert the specified variable and attach it to the specified file.
+     *  @param theFile The FileTransferObject to which to attach the file.
+     *  @param theVariable The VariableTransferObject to attach
+     *  @return true on success, false or failure
+     */
     public boolean insertVariable(FileTransferObject theFile, VariableTransferObject theVariable);
+    /**
+      *  Retrieve an iterator for all the variables matching the criteria in the search map.
+      *  @param searchMap The HashMap containing the search criteria.
+      *  @return the requested iterator
+      */
     public Iterator<VariableTransferObject> getVariables(HashMap<String, String> searchMap);
+    /**
+      *  Retrieve an iterator for all the variables attached to the specified file.
+      *  @param theFile the file for which to retrieve the variables.
+      *  @return the requested iterator
+      */
     public Iterator<VariableTransferObject> getVariables(FileTransferObject theFile);
+    /**
+      *  Update the variable specified by the id with the data in the VariableTransferObject
+      *  @param theVariable The data to use for the update.
+      *  @param variableId the id of the Collection to update.
+      *  @return boolean for success or failure.
+      */
     public boolean updateVariable(VariableTransferObject theVariable, Object variableID);
+    /**
+     *  Delete the variable specified in the VariableTransferObject.
+     *  @param theVariable The VariableTransferObject to delete.
+     *  @return the number of objects deleted.
+     */
     public int deleteVariable(VariableTransferObject theVar);
+    /**
+     *  Delete the variable selected by the search map.
+     *  @param searchMap The HashMap containing the search criteria.
+     *  @return the number of objects deleted.
+     */
     public int deleteVariable(HashMap<String, String> searchMap);
 }
 
