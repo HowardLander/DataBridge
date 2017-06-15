c4045d29358fad1f349b91fab157c97be2a1c72e
Howard Lander <howard@renci.org>
2017-06-15 12:30:49 -0400

    Added the setLogger call and some much needed documentation

--- persistence/src/main/java/org/renci/databridge/persistence/metadata/SignatureProcessor.java
+++ persistence/src/main/java/org/renci/databridge/persistence/metadata/SignatureProcessor.java
@@ -1,5 +1,7 @@
 package org.renci.databridge.persistence.metadata;
 import org.renci.databridge.persistence.metadata.*;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 /**
  * This is the interface for classes that are executed by the relavance engine to extract
  * a signature from a collection transfer object.  The signature is still expressed as a 
@@ -15,5 +17,24 @@ import org.renci.databridge.persistence.metadata.*;
  */
 public interface SignatureProcessor {
 
-    CollectionTransferObject extractSignature(CollectionTransferObject theCollection, String params);
+  /**
+   * Process a CollectionTransferObject and return a CollectionTransferObject whose metadata
+   * represents a signature derived from the original metadata.
+   * 
+   * @param theCollection The CTO representing the original metadata
+   * @param params A string containing key value pairs in the format
+   *
+   *               key1,value1|key2,value2
+   *
+   *        These paramters are specific to the implementing class
+   * 
+   * @return A collection transfer object containing a metadata signature that the method invocation
+   *         has produced from the original data
+   */
+   CollectionTransferObject extractSignature(CollectionTransferObject theCollection, String params);
+
+  /**
+   * Set a logger (e.g., parent's logger).
+   */
+  public void setLogger (Logger logger);
 }
